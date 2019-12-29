c0fd30705eb6d68295ae30811dc92c1b9120cead
moinr <moinmemon55547@gmail.com>
2019-12-29 19:21:04 +0530

    Update

---
+++ unit-2/VehicleTest.java
@@ -0,0 +1,23 @@
+interface Vehicle{
+    void numWheels(); 
+}
+class Car implements Vehicle{
+    @Override
+    public void numWheels(){
+        System.out.println("Car can have 4 wheels ");
+    }
+}
+class Truck implements Vehicle{
+    @Override
+    public void numWheels(){
+        System.out.println("Truck can have 6,10,12 or 18 wheels ");
+    }
+}
+public class VehicleTest{
+    public static void main(String[] args) {
+        Car c = new Car();
+        c.numWheels();
+        Truck t = new Truck(); 
+        t.numWheels();
+    }
+}
