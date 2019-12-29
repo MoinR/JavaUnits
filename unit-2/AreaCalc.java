ae6adb2094f31362aac251c9432166314456b8c7
moinr <moinmemon55547@gmail.com>
2019-12-29 20:01:13 +0530

    Update

--- unit-2/AreaCalc.java
+++ unit-2/AreaCalc.java
@@ -1,18 +1,25 @@
 abstract class Calculation{
-    abstract void area(); 
+    abstract void area(int x, int y); 
 }
 class Rectangle extends Calculation{
     @Override
-    public void area(){
-        System.out.println("");
+    public void area(int length, int breadth ){
+        System.out.println("Area of Rectangle : " + length * breadth);
     }
 }
 class Triangle extends Calculation{
     @Override
-    public void area(){
-        
+    public void area(int base, int height){
+        System.out.println("Area of triangle : " + (base * height) / 2);
     }
 }
 
 public class AreaCalc {
+    public static void main(String[] args) {
+        Rectangle r = new Rectangle(); 
+        r.area(5, 10);
+
+        Triangle t = new Triangle(); 
+        t.area(10,15);
+    }    
 }
